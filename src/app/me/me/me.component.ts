71dda672cb053ed468cec3233174c527a13084a9
penghui <ichpenghui@gmail.com>
2018-08-25 21:02:28 +0200

    add version to me

--- src/app/me/me/me.component.ts
+++ src/app/me/me/me.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const { version: appVersion } = require('../../../../package.json');
+
 @Component({
   selector: 'mst-me',
   templateUrl: './me.component.html',
   styleUrls: ['./me.component.scss']
 })
 export class MeComponent implements OnInit {
+  appVersion = appVersion;
 
   constructor() { }
 
