6b74387b97cc628785b72d002a9a8a4b0c911aff
penghui <ichpenghui@gmail.com>
2018-09-02 10:02:09 +0200

    highlight achieved after a week

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Unsub } from '../../../static/class/unsub';
 import { ROUTES } from '../../../static/routes';
 import { InputService } from '../../services/input.service';
+import { MstLocalStorage } from '../../../model/storage';
+import { differenceInCalendarDays } from 'date-fns';
 
 @Component({
   selector: 'mst-navigation',
@@ -12,16 +14,27 @@ import { InputService } from '../../services/input.service';
 export class NavigationComponent extends Unsub implements OnInit {
   hideNavigation = false;
   routes = ROUTES;
+  isAchievedHighlighted: boolean;
 
   constructor(private inputService: InputService) {
     super();
   }
 
   ngOnInit() {
+    this.isAchievedHighlighted = this.shouldHighlight();
     this.addSubscription(
       this.inputService.getFocusStatus().subscribe(focus => {
         this.hideNavigation = focus;
       })
     );
   }
-}
+
+  goToAchieved() {
+    MstLocalStorage.set('visitAchievedDate', Date.now());
+  }
+
+  private shouldHighlight(): boolean {
+    const date = MstLocalStorage.get('visitAchievedDate');
+    return date && differenceInCalendarDays(date, Date.now()) >= 7;
+  }
+ }
